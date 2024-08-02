d1d9b8673c616b7ab92a90b49877b27a20d4114a
fagiothree <32491856+fagiothree@users.noreply.github.com>
2024-08-02 16:48:04 +0200

    Create parenttext_models.py

---
+++ models/parenttext_models.py
@@ -0,0 +1,5 @@
+from rpft.parsers.creation.datarowmodel import DataRowModel
+from rpft.parsers.common.rowparser import ParserModel
+from parenttext_pipeline.models.parenttext_models import *
+from typing import List
+
